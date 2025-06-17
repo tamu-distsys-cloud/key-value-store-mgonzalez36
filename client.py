7e26029f67fda63d234f71cdd97218ea17a353ff
mgonzalez36 <mgonzalez36@tamu.edu>
2025-06-17 16:17:25 -0500

    Task 1 and 2 passing (5/7)

--- client.py
+++ client.py
@@ -12,8 +12,19 @@ class Clerk:
     def __init__(self, servers: List[ClientEnd], cfg):
         self.servers = servers
         self.cfg = cfg
+        self.serverIdx = 0
+        # Your definitions here
 
-        # Your definitions here.
+        ### Task 2 ###
+        # Generate random client ID to ensure unique reqs
+        self.clientID = nrand()
+        # Get server id
+        self.reqID = 0
+    def getNextReqID(self):
+        # Generate the next incoming request ID
+        self.reqID += 1
+        return self.reqID
+        ### Task 2 ###
 
     # Fetch the current value for a key.
     # Returns "" if the key does not exist.
@@ -26,8 +37,24 @@ class Clerk:
     # The types of args and reply (including whether they are pointers)
     # must match the declared types of the RPC handler function's
     # arguments in server.py.
+
     def get(self, key: str) -> str:
         # You will have to modify this function.
+        # Fetching the value in the face of no errors
+        requestID = self.getNextReqID()
+        args = GetArgs(key, self.clientID, requestID)
+
+        # Keep trying forever until a response is received
+        while True:
+            try:
+                # Make a call to the available server
+                reply = self.servers[self.serverIdx].call("KVServer.Get", args)
+                if reply is not None and hasattr(reply, 'value'):
+                    # If the reply from the server is not empty then return the fetched val
+                    return reply.value
+            except Exception as e:
+                # If an error is caught, then continue trying
+                pass
         return ""
 
     # Shared by Put and Append.
@@ -39,8 +66,19 @@ class Clerk:
     # The types of args and reply (including whether they are pointers)
     # must match the declared types of the RPC handler function's
     # arguments in server.py.
+
     def put_append(self, key: str, value: str, op: str) -> str:
         # You will have to modify this function.
+        requestID = self.getNextReqID()
+        args = PutAppendArgs(key, value, self.clientID, requestID)
+        # Keep trying forever until a response is achieved
+        while True:
+            try:
+                reply = self.servers[self.serverIdx].call("KVServer." + op, args)
+                if reply is not None and hasattr(reply, 'value'):
+                    return reply.value
+            except Exception as e:
+                pass
         return ""
 
     def put(self, key: str, value: str):
--- server.py
+++ server.py
@@ -12,9 +12,11 @@ def debug(format, *args):
 # Put or Append
 class PutAppendArgs:
     # Add definitions here if needed
-    def __init__(self, key, value):
+    def __init__(self, key, value, clientID = None, requestID = None):
         self.key = key
         self.value = value
+        self.clientID = clientID
+        self.requestID = requestID
 
 class PutAppendReply:
     # Add definitions here if needed
@@ -23,8 +25,10 @@ class PutAppendReply:
 
 class GetArgs:
     # Add definitions here if needed
-    def __init__(self, key):
+    def __init__(self, key, clientID = None, requestID = None):
         self.key = key
+        self.clientID = clientID
+        self.requestID = requestID
 
 class GetReply:
     # Add definitions here if needed
@@ -35,26 +39,109 @@ class KVServer:
     def __init__(self, cfg):
         self.mu = threading.Lock()
         self.cfg = cfg
-
         # Your definitions here.
+        self.kv_store = {}
+
+        ### Task 2 ###
+        self.completedRequests = {}
+        ### Task 2 ###
 
     def Get(self, args: GetArgs):
-        reply = GetReply(None)
+        with self.mu:
+            ### Task 2 ###
+            # For task 2, the use of unique client and request IDs will be required
+            # so that we are able to uniquely identify them for single executions
+            if args.clientID is not None and args.requestID is not None:
+                requestKey = (args.clientID, args.requestID)
+                # If both client and req keys are not empty, then package then as request key obj
+                if requestKey in self.completedRequests:
+                    # If the request key has already been completed, then reply with the cached value
+                    # this ensures single execution
+                    cachedVal = self.completedRequests[requestKey]
+                    reply = GetReply("")
+                    reply.value = cachedVal
+                    return reply
+                
+            # If the request ID is unique and not found in cache, then fulfill the request
+            value = self.kv_store.get(args.key, "")
 
-        # Your code here.
+            # Cache request key to ensure single execution
+            if args.clientID is not None and args.requestID is not None:
+                requestKey = (args.clientID, args.requestID)
+                self.completedRequests[requestKey] = value
 
-        return reply
+            # Generate reply and return
+            reply = GetReply("")
+            reply.value = value
+            return reply
+        
+
+            # value = self.kv_store.get(args.key, "")
+            # reply = GetReply("")
+            # reply.value = value
+            # debug("Get:(%s, %s)", args.key, value)
+            # # Your code here.
+            # return reply
 
     def Put(self, args: PutAppendArgs):
-        reply = PutAppendReply(None)
+        with self.mu:
+            # Check for duplicate request
+            if args.clientID is not None and args.requestID is not None:
+                # Construct requestKey
+                requestKey = (args.clientID, args.requestID)
+                if requestKey in self.completedRequests:
+                    # Fetch the cached value and return the reply
+                    cachedVal = self.completedRequests[requestKey]
+                    reply = PutAppendReply("")
+                    reply.value = cachedVal
+                    return reply
+                
+            # If the request key is unique, then cache it, append and reply
+            self.kv_store[args.key] = args.value
+            appendResult = ""
 
-        # Your code here.
+            if args.clientID is not None and args.requestID is not None:
+                requestKey = (args.clientID, args.requestID)
+                self.completedRequests[requestKey] = appendResult
+            
+            reply = PutAppendReply("")
+            reply.value = appendResult
+            return reply
 
-        return reply
+            # self.kv_store[args.key] = args.value
+            # reply = PutAppendReply("")
+            # reply.value = ""
+            # debug("Put(%s, %s)", args.key, args.value)
+            # return reply
 
     def Append(self, args: PutAppendArgs):
-        reply = PutAppendReply(None)
+        with self.mu:
+            if args.clientID is not None and args.requestID is not None:
+                requestKey = (args.clientID, args.requestID)
+                if requestKey in self.completedRequests:
+                    cachedVal = self.completedRequests[requestKey]
+                    reply = PutAppendReply("")
+                    reply.value = cachedVal
+                    return reply
 
-        # Your code here.
+            # Get  current val and put empty string placeholder
+            oldVal = self.kv_store.get(args.key, "")
 
-        return reply
+            # Append newVal to oldVal
+            self.kv_store[args.key] = oldVal + args.value
+
+            # Return oldVal
+
+            if args.clientID is not None and args.requestID is not None:
+                requestKey = (args.clientID, args.requestID)
+                self.completedRequests[requestKey] = oldVal
+
+            reply = PutAppendReply("")
+            reply.value = oldVal
+            return reply
+
+            # reply = PutAppendReply("")
+            # reply.value = oldVal
+            # debug("Append: key %s, oldVal %s, newVal %s", args.key, oldVal, self.kv_store[args.key])
+            # # Your code here.
+            # return reply
